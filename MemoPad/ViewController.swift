4c5d6f9966a585a4f4b68449df6f9ceb595a6e36
Reina Kobayashi <kinokodaizukinako@gmail.com>
2020-09-15 13:27:18 +0900

    メモアプリ

---
+++ MemoPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoPad
+//
+//  Created by 小林玲衣奈 on 2020/09/03.
+//  Copyright © 2020 小林玲衣奈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
